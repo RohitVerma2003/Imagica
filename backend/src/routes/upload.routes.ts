eabef716edf8f872f03931f10dffe267713acff0
RohitVerma2003 <rvermajio78300@gmail.com>
2026-04-24 12:53:07 +0530

    Rate Limit added

--- backend/src/routes/upload.routes.ts
+++ backend/src/routes/upload.routes.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { upload } from "../config/multer";
 import { uploadImage } from "../controllers/upload.controller";
 import { validateImageRequest } from "../middleware/validation.middleware";
+import { uploadLimiter } from "../middleware/ratelimit.middleware";
 
 const router = Router();
 
-router.post("/", upload.single("image"), validateImageRequest, uploadImage);
+router.post("/", uploadLimiter, upload.single("image"), validateImageRequest, uploadImage);
 
 export default router;
